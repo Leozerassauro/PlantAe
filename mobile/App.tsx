42377e38819f84fa86ad83317c7b9cc3b8ffd9cb
Leonardo Girardi <leo.girardi97@hotmail.com>
2023-04-26 19:36:50 -0300

    installed native base

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable camelcase */
+import { View, Text, StatusBar } from 'react-native'
+import { useFonts, Imprima_400Regular } from '@expo-google-fonts/imprima'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Imprima_400Regular })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <Text>Open up App.tsx to start working on your app!</Text>
+      ) : (
+        <View />
+      )}
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
